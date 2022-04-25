f7181a9f9e202a44444e8b190896d1514747e189
rnjsrnrdnjs <090tree@gmail.com>
2022-04-25 21:14:37 +0900

    123

--- imagelabel/src/components/main.js
+++ imagelabel/src/components/main.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef, ProgressBar } from "react";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import ReactSlider from "react-slider";
 
 const frameList = [
   {
@@ -19,14 +20,44 @@ const frameList = [
   },
 ];
 
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
 export default function Main({ updateLogedIn, loginState }) {
   const navigate = useNavigate();
   useEffect(() => {
     if (loginState === false) navigate("/");
   }, []);
 
-  let now = 0;
+  const [delay, setDelay] = useState(null);
+  const [now, setNow] = useState(0);
   const [selectList, setSelectList] = useState([]);
+  const [playChk, setPlayChk] = useState(false);
+
+  useInterval(
+    () => {
+      setNow(now + 1);
+      console.log(now);
+    },
+    now < frameList.length - 1 ? delay : null
+  );
 
   return (
     <div
@@ -71,13 +102,24 @@ export default function Main({ updateLogedIn, loginState }) {
           {">"}
         </div>
       </div>
-      <div style={{ marginTop: "10px", maxWidth: "80vw", maxHeight: "50vh" }}>
+      <div style={{ marginTop: "10px", maxWidth: "80vw", maxHeight: "40vh" }}>
         <img
           src="/logo512.png"
-          style={{ maxWidth: "80vw", maxHeight: "50vh" }}
+          style={{ maxWidth: "80vw", maxHeight: "40vh" }}
         />
       </div>
-      <div style={{ marginTop: "10px", width: "80vw" }}></div>
+
+      <input
+        type="range"
+        value={now}
+        style={{ width: "80vw", height: "20px" }}
+        max={frameList.length - 1}
+        min={0}
+        onChange={(e) => {
+          setNow(parseInt(e.target.value));
+        }}
+      />
+
       <div
         style={{
           marginTop: "10px",
@@ -88,12 +130,32 @@ export default function Main({ updateLogedIn, loginState }) {
       >
         {frameList.map((list, idx) => (
           <div
-            style={{
-              maxWidth: "20vh",
-              maxHeight: "20vh",
-            }}
+            style={
+              now !== idx
+                ? {
+                    maxWidth: "20vh",
+                    maxHeight: "20vh",
+                    justifyContent: "center",
+                    alignContent: "center",
+                    alignItems: "center",
+                  }
+                : {
+                    border: "3px solid #828282",
+                    maxWidth: "20vh",
+                    justifyContent: "center",
+                    alignContent: "center",
+                    alignItems: "center",
+                    maxHeight: "20vh",
+                  }
+            }
           >
-            <img src={list.src} />
+            <img
+              src={list.src}
+              style={{
+                maxWidth: "20vh",
+                maxHeight: "20vh",
+              }}
+            />
           </div>
         ))}
       </div>
@@ -105,6 +167,11 @@ export default function Main({ updateLogedIn, loginState }) {
             border: "1px solid #828282",
             padding: "10px",
           }}
+          onClick={() => {
+            setDelay(null);
+            setPlayChk(false);
+            if (now >= 1) setNow(now - 1);
+          }}
         >
           이전 프레임
         </div>
@@ -115,8 +182,18 @@ export default function Main({ updateLogedIn, loginState }) {
             border: "1px solid #828282",
             padding: "10px",
           }}
+          onClick={() => {
+            if (playChk === false) {
+              setPlayChk(true);
+              setDelay(null);
+              setDelay(300);
+            } else {
+              setPlayChk(false);
+              setDelay(null);
+            }
+          }}
         >
-          재생
+          {playChk === false ? "재생" : "멈춤"}
         </div>
         <div
           style={{
@@ -124,6 +201,11 @@ export default function Main({ updateLogedIn, loginState }) {
             borderRadius: "10px",
             border: "1px solid #828282",
             padding: "10px",
+          }}
+          onClick={() => {
+            setDelay(null);
+            setPlayChk(false);
+            if (frameList.length - 1 > now) setNow(now + 1);
           }}
         >
           다음프레임
@@ -135,19 +217,32 @@ export default function Main({ updateLogedIn, loginState }) {
             border: "1px solid #828282",
             padding: "10px",
           }}
+          onClick={() => {
+            setDelay(null);
+            setPlayChk(false);
+            setSelectList(selectList.concat(frameList[now]));
+          }}
         >
           선택
         </div>
       </div>
-      <div>
+      <div
+        style={{
+          marginTop: "10px",
+          overflow: "scroll",
+          maxWidth: "80vw",
+          maxHeight: "20vh",
+        }}
+      >
         {selectList.map((list, idx) => (
-          <div
-            style={{
-              maxWidth: "20vh",
-              maxHeight: "20vh",
-            }}
-          >
-            <img src={list.src} />
+          <div>
+            <img
+              src={list.src}
+              style={{
+                maxWidth: "20vh",
+                maxHeight: "20vh",
+              }}
+            />
           </div>
         ))}
       </div>
